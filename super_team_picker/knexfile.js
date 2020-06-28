57da55723bc1c5a83176d80ce4de300d22e89669
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-06-28 15:25:16 -0700

    added solution for super team picker

---
+++ super_team_picker/knexfile.js
@@ -0,0 +1,14 @@
+  
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "super_team_picker"
+    },
+    migrations: {
+      directory: "db/migrations"
+    }
+  }
+
+};
